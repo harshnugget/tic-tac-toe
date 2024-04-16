d87427b249da753b0db29ade20c033b732bbb2fb
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-04-16 14:03:23 +0100

    Pseudocode logic

---
+++ script.js
@@ -0,0 +1,13 @@
+// gameboard:
+// 	Creates board
+// 	Keeps track of board data
+
+// gameController:
+// 	Creates player with unique name and token
+// 	Keeps track of players turn
+//  Updates board
+// 	Determines if a win/lose/draw states
+
+// displayController:
+// 	Creates and updates UI elements
+// 	Renders contents of gameboard array
